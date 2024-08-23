62b158bcd67a44fefc3ee8b2345a5ef84d1eee06
Bryanth87 <tgbryan14@gmail.com>
2024-08-23 17:06:57 -0600

    add: se agrego y configuro el controller y los menus

--- org.derianhernandez.bean/Tester.java
+++ org.derianhernandez.bean/Tester.java
@@ -1,11 +1,18 @@
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Data
 @Getter
+@NoArgsConstructor
 public class Tester extends Empleado{
 
     private String tipoPruebas;
+
+    public Tester(String tipoPruebas,String nombre,int edad,double salario){
+        super(nombre,edad,salario);
+        this.tipoPruebas = tipoPruebas;
+    }
 
     @Override
     public void trabajar() {
--- org.derianhernandez.system/Main.java
+++ org.derianhernandez.system/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("waza");
+        EmpleadoView start = new EmpleadoView();
+        start.Menu();
     }
 }
