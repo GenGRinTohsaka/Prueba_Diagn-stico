e00bfbcd52bceacd143b480fc108c5b70e6f7105
danitt030 <danielitotuchez100@gmail.com>
2024-08-23 15:43:21 -0600

    feat: se agregaro el metodo encender y mostrarinformacion

--- org.javierapen.bean/Dispositivo.java
+++ org.javierapen.bean/Dispositivo.java
@@ -1,11 +1,26 @@
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public abstract class Dispositivo {
 
     private String marca;
     private String modelo;
     private int anio;
 
-    
+    public abstract void encender();
+
+    public void encender(int voltaje){
+        System.out.println("Se encendio el dispositivo con voltaje: "+voltaje+ " W");
+    }
+
+    public void mostrarInformacion(){
+        System.out.println("Marca " + marca );
+        System.out.println("Modelo " + modelo);
+        System.out.println("Año " + anio);
+    }
+
 }
